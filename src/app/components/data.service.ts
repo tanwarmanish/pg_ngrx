6e78ee433bca3e85dd5a0ae929565abd1cd93008
Manish Tanwar <0manishtanwar@gmail.com>
2023-10-27 01:25:23 +0530

    RXJS updated

--- src/app/components/data.service.ts
+++ src/app/components/data.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  tasks:any = [];
+  taskList$:BehaviorSubject<any> = new BehaviorSubject([]);
 
   constructor() { }
 
   addTask(task:string){
-    this.tasks.unshift(task);
+    const tasks = this.taskList$.getValue();
+    tasks.unshift(task);
+    this.taskList$.next(tasks);
   }
 
   removeTask(index:number){
-    this.tasks.splice(index,1);
+    const tasks = this.taskList$.getValue();
+    tasks.splice(index,1);
+    this.taskList$.next(tasks);
   }
 }
